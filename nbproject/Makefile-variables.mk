c145813b4775bf82b86d4b9cdde5d662b038968a
Simone <simone@DESKTOP-D5F42L6.homenet.telecomitalia.it>
2016-06-01 18:40:27 +0200

    First version

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=OBD.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/OBD.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=obd.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/obd.x.tar
